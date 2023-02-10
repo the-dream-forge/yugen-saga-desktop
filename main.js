377d0de842ef66b424c5563fea8f6873f779b060
Jacques <941214+jacola@users.noreply.github.com>
2023-02-10 21:39:16 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,28 @@
+var os = require("os");
+const { app, BrowserWindow, screen, Menu } = require("electron");
+const { autoUpdater, AppUpdater } = require("electron-updater");
+const path = require("path");
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    title: "Yugen Saga",
+    icon: "build/icon.png",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile("game/index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
